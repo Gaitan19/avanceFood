d1310beb0b707edbe525a4a5c570b3c26bc39f86
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-25 15:49:12 -0600

    counter area

---
+++ constants/statistics/index.js
@@ -0,0 +1,23 @@
+export const statistics = [
+  {
+    image: '/icon/reservation.png',
+    counter: 120,
+    title: 'Reservation',
+  },
+  {
+    image: '/icon/chefs.png',
+    counter: 20,
+    title: 'Master Chef',
+  },
+  {
+    image: '/icon/food.png',
+    counter: 50,
+    title: 'Food Menu',
+  },
+
+  {
+    image: '/icon/satisfy-user.png',
+    counter: 60690,
+    title: 'Satisfy User',
+  },
+];
