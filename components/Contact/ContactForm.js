97b6e8829aa3fceda5a0667494c6ff56a131cd95
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-26 19:48:50 -0600

    it was completed the section contact

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,55 +1,46 @@
 import Button from '../Button';
 
 const ContactForm = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    console.log(event);
-  };
-  //   const schema = Yup.object({
-  //     name: Yup.string().min(4, 'Name to short').required('Name is Required'),
-  //     email: Yup.string()
-  //       .email('Invalid email address')
-  //       .required('Email is Required'),
-  //     phone: Yup.number()
-  //       .positive('Must be positive number')
-  //       .integer('Write a valid number')
-  //       .typeError('Must be number'),
-  //     message: Yup.string().max(1000).min(30).required('Write a message'),
-  //   });
-
-  //   const initialValues = {
-  //     name: '',
-  //     email: '',
-  //     phone: '',
-  //     message: '',
-  //   };
-
   return (
-    <div className="Contact">
+    <div className="Contact Contact-form">
       <div className="Contact-content">
         <h2 className="Contact-title">Leave us a message</h2>
 
-        <form className="Contact-message-form" onSubmit={handleSubmit}>
-          <div className="Contact-message-input">
-            <input name="name" type="text" placeholder="First Name" />
-          </div>
-
-          <div className="Contact-message-input">
-            <input name="email" type="email" placeholder="Email Address" />
-          </div>
-
-          <div className="Contact-message-input">
-            <input name="phone" type="tel" placeholder="Phone" />
-          </div>
-
-          <div className="Contact-message-input">
+        <form className="Contact-message-form">
+          <div>
             <input
-              as="textarea"
+              className="Contact-message-input"
+              type="text"
+              placeholder="First Name"
+              required
+            />
+          </div>
+
+          <div>
+            <input
+              className="Contact-message-input"
+              type="email"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+
+          <div>
+            <input
+              className="Contact-message-input"
+              type="tel"
+              placeholder="Phone"
+              required
+            />
+          </div>
+
+          <div>
+            <textarea
+              className="Contact-message-input Contact-message-task"
               name="message"
               type="text"
               placeholder="Write message"
-              customClass="message"
+              required
             />
           </div>
 
