f1119cea56811269247ef602d457b8e2fd4b15fb
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-27 08:45:37 -0600

    complete the page contact

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -7,44 +7,35 @@ const ContactForm = () => {
         <h2 className="Contact-title">Leave us a message</h2>
 
         <form className="Contact-message-form">
-          <div>
-            <input
-              className="Contact-message-input"
-              type="text"
-              placeholder="First Name"
-              required
-            />
-          </div>
+          <input
+            className="Contact-message-input"
+            type="text"
+            placeholder="First Name"
+            required
+          />
 
-          <div>
-            <input
-              className="Contact-message-input"
-              type="email"
-              placeholder="Email Address"
-              required
-            />
-          </div>
+          <input
+            className="Contact-message-input"
+            type="email"
+            placeholder="Email Address"
+            required
+          />
 
-          <div>
-            <input
-              className="Contact-message-input"
-              type="tel"
-              placeholder="Phone"
-              required
-            />
-          </div>
+          <input
+            className="Contact-message-input"
+            type="tel"
+            placeholder="Phone"
+            required
+          />
 
-          <div>
-            <textarea
-              className="Contact-message-input Contact-message-task"
-              name="message"
-              type="text"
-              placeholder="Write message"
-              required
-            />
-          </div>
+          <textarea
+            className="Contact-message-input Contact-message-task"
+            name="message"
+            type="text"
+            placeholder="Write message"
+            required
+          />
 
-          {/* <Input type="submit" className="button secondary" value="Send Messag" /> */}
           <Button buttonType="submit" customClass="Button-fill">
             <span className="Button-fill-text">Send Message</span>
           </Button>
