d528558f2359b3a8d3c70db4069097402757dc7a
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-30 11:06:43 -0600

    left site completed

--- components/BlogDetails/CommentForm.js
+++ components/BlogDetails/CommentForm.js
@@ -1,49 +1,24 @@
-import { useState } from 'react';
-import { blogComents } from '@/constants/blogsList';
 import Button from '../Button';
 
 const CommentForm = () => {
-  const [userName, setUserName] = useState('');
-  const [userComment, setUserComment] = useState('');
-
-  const handleChangeName = (input) => {
-    const {
-      target: { value },
-    } = input;
-    setUserName(value);
-  };
-
-  const handleChangeComment = (input) => {
-    const {
-      target: { value },
-    } = input;
-    setUserComment(value);
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    blogComents.push({
-      image: blogComents[0].image,
-      name: userName,
-      text: userComment,
-    });
   };
 
   return (
     <form className="Details-form" onSubmit={handleSubmit}>
-      <h3>Leave a comment</h3>
+      <h3 className="Details-form-title">Leave a comment</h3>
 
       <div className="Details-form-container">
         <input
-          className="Details-input"
+          className="Details-input Details-input-group"
           type="text"
           placeholder="First Name"
           required
-          onChange={handleChangeName}
         />
 
         <input
-          className="Details-input"
+          className="Details-input Details-input-group"
           type="email"
           placeholder="Enter email addres"
           required
@@ -55,7 +30,6 @@ const CommentForm = () => {
         type="text"
         placeholder="Write your comment"
         required
-        onChange={handleChangeComment}
       />
 
       <Button buttonType="submit" customClass="Button-fill">
