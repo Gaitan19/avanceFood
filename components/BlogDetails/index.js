7a204caa1a43df6eba87543db908a6e10d9dbbc6
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-30 08:22:58 -0600

    starting to make blog details page

---
+++ components/BlogDetails/index.js
@@ -0,0 +1,12 @@
+import Banner from '../Banner';
+import Layout from '../Layout';
+
+const BlogDetails = () => {
+  return (
+    <Layout headPageTitle="BlogDetails">
+      <Banner title="Blog" page="Blog Details" />
+    </Layout>
+  );
+};
+
+export default BlogDetails;
