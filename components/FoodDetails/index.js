5206a113fb36ced0d99f26a50d783eb3a4bc2d79
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-31 15:41:52 -0600

    it was starting to create the section food details

--- components/FoodDetails/index.js
+++ components/FoodDetails/index.js
@@ -5,6 +5,14 @@ const FoodDetails = () => {
   return (
     <Layout headPageTitle="Food details">
       <Banner title="Food details" page="Food details" />
+      <div className="Food-details">
+        <div className="Food-details-content">
+          <div className="Food-details-container">
+            <div className="Food-details-left"></div>
+            <div className="Food-details-right"></div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
