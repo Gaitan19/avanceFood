dfd9ef00fe192bf1755438b8d475816c04f71ceb
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-31 15:29:59 -0600

    starting to create the food details page

---
+++ components/FoodDetails/index.js
@@ -0,0 +1,12 @@
+import Banner from '../Banner';
+import Layout from '../Layout';
+
+const FoodDetails = () => {
+  return (
+    <Layout headPageTitle="Food details">
+      <Banner title="Food details" page="Food details" />
+    </Layout>
+  );
+};
+
+export default FoodDetails;
