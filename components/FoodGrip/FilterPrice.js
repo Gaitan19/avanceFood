35844ed531a26d47db99d9678a6a6fbad2b84d02
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-31 10:17:58 -0600

    it was created the slider from item left in food grip page

--- components/FoodGrip/FilterPrice.js
+++ components/FoodGrip/FilterPrice.js
@@ -1,7 +1,48 @@
+import Slider from '@mui/material/Slider';
+import { useState } from 'react';
+import Typography from '@mui/material/Typography';
 import FoodLeftItem from './FoodLeftItem';
 
 const FilterPrice = () => {
-  return <FoodLeftItem title="Filter by price"></FoodLeftItem>;
+  const [valueSlider, setValueSlider] = useState([0, 200]);
+
+  const minDistance = 200;
+
+  const handleChange1 = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setValueSlider([
+        Math.min(newValue[0], valueSlider[1] - minDistance),
+        valueSlider[1],
+      ]);
+    } else {
+      setValueSlider([
+        valueSlider[0],
+        Math.max(newValue[1], valueSlider[0] + minDistance),
+      ]);
+    }
+  };
+
+  const sliderLable = (value) => `$${value}`;
+
+  return (
+    <FoodLeftItem title="Filter by price">
+      <Slider
+        value={valueSlider}
+        onChange={handleChange1}
+        valueLabelDisplay="on"
+        max={1000}
+        min={0}
+        step={50}
+        disableSwap
+        className="Food-slider"
+        valueLabelFormat={sliderLable}
+      />
+    </FoodLeftItem>
+  );
 };
 
 export default FilterPrice;
